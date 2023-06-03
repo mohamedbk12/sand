43742be771851bbd3919244e27f5269df67f6b84
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-03 11:20:04 +0100

    e15

---
+++ Configuration/TCA/Overrides/tt_content_element_e15imagearea.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e15imagearea'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e15imagearea'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-15 Image area',
+        'sandsitepackage_e15imagearea',
+        'sandsitepackage-e15imagearea'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e15imagearea'] = 'sandsitepackage-e15imagearea';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e15imagearea'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e15imagearea'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            slider_type,link_text,header,subheader,bodytext,image,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e15imagearea']['columnsOverrides'] = [
+    'link_text' => [
+        'label' => 'Number',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
+        ],
+    ], 
+    'header' => [
+        'label' => 'Top header',
+    ],
+    'slider_type' => [
+        'exclude' => true,
+        'label' => 'Box type',
+        'config' => [
+            'type' => 'select',
+            'renderType' => 'selectSingle',
+            'items' => [
+                ['Big box', '1'],
+                ['Small box', '2'],
+            ],
+            'default' => ''
+        ]
+    ],
+];
+
