ff70bae95812779941854871095df84907d9a8b3
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-04 22:30:25 +0100

    e17

---
+++ Configuration/TCA/Overrides/tt_content_element_e17imageswithtitle.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e17imageswithtitle'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e17imageswithtitle'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-17 Images with titles',
+        'sandsitepackage_e17imageswithtitle',
+        'sandsitepackage-e17imageswithtitle'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e17imageswithtitle'] = 'sandsitepackage-e17imageswithtitle';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e17imageswithtitle'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e17imageswithtitle'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+           tx_sand_sitepackage_teaser_item,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e17imageswithtitle']['columnsOverrides'] = [
+    'tx_sand_sitepackage_teaser_item' => [
+        'config' => [
+            'overrideChildTca' => [
+                'types' => [
+                    '1' => [
+                        'showitem' => '
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                            color_select,header,image,
+                            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                            --palette--;;hiddenLanguagePalette,
+                        '
+                    ],
+                ],
+                'columns' => [
+                    'color_select' => [
+                        'label' => 'Background color',
+                        'config' => [
+                            'type' => 'select',
+                            'renderType' => 'selectSingle',
+                            'items' => [
+                                ['top-left', 'top-left'],
+                                ['top-right', 'top-right'],
+                                ['bottom-left', 'bottom-left'],
+                                ['bottom-right', 'bottom-right'],
+                            ],
+                        ]
+                    ],
+                ]
+            ],
+        ],
+    ],
+];
