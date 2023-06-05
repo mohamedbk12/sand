cb74e0b35d3db5241ec88bc9567cf406d435907b
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-05 21:57:45 +0100

    e19

---
+++ Configuration/TCA/Overrides/tt_content_element_e19contentlongimg.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e19contentlongimg'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e19contentlongimg'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-19 content long image',
+        'sandsitepackage_e19contentlongimg',
+        'sandsitepackage-e19contentlongimg'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e19contentlongimg'] = 'sandsitepackage-e19contentlongimg';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e19contentlongimg'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e19contentlongimg'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+           image,tx_sand_sitepackage_teaser_item,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e19contentlongimg']['columnsOverrides'] = [
+    'tx_sand_sitepackage_teaser_item' => [
+        'config' => [
+            'overrideChildTca' => [
+                'types' => [
+                    '1' => [
+                        'showitem' => '
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                            color_select,image_position,header,bodytext,
+                            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                            --palette--;;hiddenLanguagePalette,
+                        '
+                    ],
+                ],
+                'columns' => [
+                    'color_select' => [
+                        'label' => 'Hover color',
+                        'config' => [
+                            'type' => 'select',
+                            'renderType' => 'selectSingle',
+                            'items' => [
+                                ['bleu', ''],
+                                ['yellow', 'trugreen-hover'],
+                            ],
+                        ]
+                    ],
+                    'image_position' => [
+                        'label' => 'Hover color',
+                        'config' => [
+                            'type' => 'select',
+                            'renderType' => 'selectSingle',
+                            'items' => [
+                                ['col-span-2', 'col-span-2'],
+                                ['col-span-3', 'col-span-3'],
+                                ['col-span-4', 'col-span-4'],
+                                ['col-span-5', 'col-span-5'],
+                                ['col-span-6', 'col-span-6'],
+                                ['col-span-12', 'col-span-12'],
+                            ],
+                        ]
+                    ],
+                ]
+            ],
+        ],
+    ],
+];
