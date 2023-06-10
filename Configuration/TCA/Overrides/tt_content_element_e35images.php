8812af58373d36c958923f9703a8185ef93e3296
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-10 23:08:50 +0100

    e35

---
+++ Configuration/TCA/Overrides/tt_content_element_e35images.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e35images'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e35images'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-35 Images',
+        'sandsitepackage_e35images',
+        'sandsitepackage-e35images'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e35images'] = 'sandsitepackage-e35images';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e35images'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e35images'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+           image,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e35images']['columnsOverrides'] = [
+    'image' => [
+        'label' => $ll . 'image',
+        'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+            'image',
+            [
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference'
+                ],
+                'overrideChildTca' => [
+                    'types' => [
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+                            'showitem' => '
+                                title,
+                                alternative,
+                                crop,
+                                --palette--;;filePalette
+                            '
+                        ],
+                    ],
+                ],
+                'minitems' => 0,
+                'maxitems' => 4,
+            ],
+            ('png,jpg,svg')
+            /* Enable this line and delete the line above to enable more images extensions */
+            // $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+        ),
+    ],
+];
