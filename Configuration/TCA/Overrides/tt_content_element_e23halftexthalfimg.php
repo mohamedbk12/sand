1c1650532c82c1b16ca659f63ea6e3262b20fc1d
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-10 09:30:48 +0100

    E23

---
+++ Configuration/TCA/Overrides/tt_content_element_e23halftexthalfimg.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e23halftexthalfimg'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e23halftexthalfimg'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-23 half text half image',
+        'sandsitepackage_e23halftexthalfimg',
+        'sandsitepackage-e23halftexthalfimg'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e23halftexthalfimg'] = 'sandsitepackage-e23halftexthalfimg';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e23halftexthalfimg'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e23halftexthalfimg'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+           image,header,bodytext,link,link_text,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
