51ddcfbf0bb5b2e94ec56aacccf9d0ea4e96664c
mohamed boukattaya <boukatayamed1@gmail.com>
2023-06-21 19:46:46 +0100

    E40

---
+++ Configuration/TCA/Overrides/tt_content_element_e40questions.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the package Meddev/sand_sitepackage
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:sand_sitepackage/Resources/Private/Language/locallang_db.xlf:';
+
+/***************
+ * Add Content Element
+ */
+if (!isset($GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e40questions'])) {
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e40questions'] = [];
+}
+
+/***************
+ * Add content element to selector list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'E-40 Questions',
+        'sandsitepackage_e40questions',
+        'sandsitepackage-e40questions'
+    ],
+    '--div--',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['sandsitepackage_e40questions'] = 'sandsitepackage-e40questions';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e40questions'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e40questions'],
+    [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            tx_sand_sitepackage_teaser_item,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                        rowDescription,        
+            --div--;spacement,
+            space_before_class,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+
+
+
+$GLOBALS['TCA']['tt_content']['types']['sandsitepackage_e40questions']['columnsOverrides'] = [
+    'tx_sand_sitepackage_teaser_item' => [
+        'config' => [
+            'overrideChildTca' => [
+                'types' => [
+                    '1' => [
+                        'showitem' => '
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                            ,image,header,bodytext,
+                            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                            --palette--;;hiddenLanguagePalette,
+                        '
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
